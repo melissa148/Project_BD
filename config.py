f0a748127c2701666e72b3e76e92f8e498d7d499
melissa148 <136941622+melissa148@users.noreply.github.com>
2024-11-25 18:36:13 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,10 @@
+class Config:
+    # Configuración para MySQL (relacional)
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://user:password@localhost/db_name'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Configuración para Neo4j (no relacional)
+    NEO4J_URI = "bolt://localhost:7687"
+    NEO4J_USER = "neo4j"
+    NEO4J_PASSWORD = "password"
 
