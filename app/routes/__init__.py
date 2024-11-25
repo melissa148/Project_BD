9689e31daf948369840cb9de595ef8b14bed1798
melissa148 <136941622+melissa148@users.noreply.github.com>
2024-11-25 18:39:49 -0500

    Update __init__.py

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,1 +1,7 @@
+from .users import user_bp
+from .posts import post_bp
+
+def init_routes(app):
+    app.register_blueprint(user_bp, url_prefix='/api')
+    app.register_blueprint(post_bp, url_prefix='/api')
 
