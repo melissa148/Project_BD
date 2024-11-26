40a3e0da4ed321ac7e2694145a098d99cd89d158
melissa148 <136941622+melissa148@users.noreply.github.com>
2024-11-25 20:32:38 -0500

    Update __init__.py

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,7 +1,12 @@
 from .users import user_bp
 from .posts import post_bp
+from .followers import follow_bp
+from .notifications import notification_bp
 
 def init_routes(app):
-    app.register_blueprint(user_bp, url_prefix='/api')
-    app.register_blueprint(post_bp, url_prefix='/api')
+    # Registrar los Blueprints de cada módulo
+    app.register_blueprint(user_bp, url_prefix='/api/users')
+    app.register_blueprint(post_bp, url_prefix='/api/posts')
+    app.register_blueprint(follow_bp, url_prefix='/api/follow')
+    app.register_blueprint(notification_bp, url_prefix='/api/notifications')
 
