1f00d30e538fcf0b0406467e80e7a60be4509714
melissa148 <136941622+melissa148@users.noreply.github.com>
2024-11-25 20:55:57 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,28 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
+from flask_migrate import Migrate  # Importar Flask-Migrate para las migraciones
 from app.neo4j_connection import Neo4jConnection
 
 # Inicializar la base de datos SQLAlchemy (MySQL)
 db = SQLAlchemy()
 
+# Inicializar Flask-Migrate
+migrate = Migrate()
+
 # Inicializar la conexión a Neo4j
-neo4j = Neo4jConnection(uri='bolt://localhost:7687', user='neo4j', password='password')
+neo4j = Neo4jConnection(uri='bolt://50.16.13.108:7687', user='neo4j', password='pond-dolly-shot')
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('config.Config')
+    app.config.from_object('config.Config')  # Configurar la aplicación con los valores en config.py
     
     # Inicializar la base de datos SQLAlchemy
     db.init_app(app)
-
+    
+    # Inicializar Flask-Migrate con la app y db
+    migrate.init_app(app, db)
+    
     # Configurar Flask-RESTful para las APIs
     api = Api(app)
 
@@ -24,4 +31,3 @@ def create_app():
     init_routes(app)
 
     return app
-
